f7054d1e7d58857aca70266ec8f6e81710f1537a
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2023-12-06 16:54:48 -0300

    fix git commands

---
+++ production_run.py
@@ -0,0 +1,33 @@
+import webbrowser
+import time
+import subprocess
+import os
+from dotenv import load_dotenv
+from main import request_data, process_and_publish_responses, update_sheets_dataset
+
+def main():
+    load_dotenv()
+    MAIN_GROUP_CHAT_ID = os.environ["MAIN_GROUP_CHAT_ID"]
+
+    request_data()
+
+    process_and_publish_responses(MAIN_GROUP_CHAT_ID)
+
+    update_sheets_dataset()
+    
+    subprocess.run(["git", "add", "."], check=True)
+    subprocess.run(["git", "commit", "-m", "Daily update"], check=True)
+    subprocess.run(["git", "push", "origin", "main"], check=True)
+
+    print(">> WAITING 20s FOR THE PROCESS TO FINISH")
+    time.sleep(20)
+
+    url = "https://bit.ly/planilhaJuniorZone1"
+    webbrowser.open(url)
+
+    input(">> PRESS ENTER TO EXIT")
+    exit()
+
+
+if __name__ == "__main__":
+    main()
