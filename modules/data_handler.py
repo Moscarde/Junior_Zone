6c125b462fa7a03d7fdb1f878d7e66647f037aef
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2023-12-07 23:15:22 -0300

    refactor update_google_sheets_dataset

--- modules/data_handler.py
+++ modules/data_handler.py
@@ -10,8 +10,9 @@ class DataHandler:
         self.filtered_dfs = self.process_dataset(self.base_dataset)
 
     def process_dataset(self, base_dataset):
-        df_jr = self.apply_exclusion_filters(
-            base_dataset, ["PLENO", "SÊNIOR", "SENIOR", "SR", "PL", "III", "lll", "ll", "II"]
+        df_jr = apply_exclusion_filters(
+            base_dataset,
+            ["PLENO", "SÊNIOR", "SENIOR", "SR", "PL", "III", "lll", "ll", "II"],
         )
 
         df_jr = df_jr.sort_values(by="state")
@@ -39,11 +40,11 @@ class DataHandler:
             "DevOps",
         ]
 
-        df_dados = self.apply_search_filters(df_jr, dados_filter)
+        df_dados = apply_search_filters(df_jr, dados_filter)
         df_dados_remote = df_dados[(df_dados["workplace_type"] == "remote")]
         df_dados_hybrid = df_dados[(df_dados["workplace_type"] == "hybrid")]
 
-        df_dev = self.apply_search_filters(df_jr, dev_filter)
+        df_dev = apply_search_filters(df_jr, dev_filter)
         df_dev_remote = df_dev[(df_dev["is_remote_work"] == True)]
         df_dev_hybrid = df_dev[(df_dev["workplace_type"] == "hybrid")]
 
@@ -54,49 +55,67 @@ class DataHandler:
             "dev_hybrid": df_dev_hybrid,
         }
 
-    def apply_exclusion_filters(self, df, filters):
-        for f in filters:
-            df = df[~df["title"].str.upper().str.contains(f.upper())]
-
-        df = df[
-            ((df["country"] == "Brasil") | (df["country"].isna()))
-            & (df["submitted"] == False)
-        ]
-
-        return df
-
-    def apply_search_filters(self, df, filters):
-        temp_df_list = []
-        for f in filters:
-            temp_df_list.append(df[df["title"].str.upper().str.contains(f.upper())])
-
-        df_final = pd.concat(temp_df_list, ignore_index=True)
-        return df_final
-
     def tag_as_submitted(self):
         self.base_dataset["submitted"] = "True"
         self.base_dataset.to_csv(f"data/responses/{self.date}.csv", index=False)
 
 
-def update_google_sheets_dataset():
-    data_files = os.listdir("data/responses/")
+def apply_exclusion_filters(df, filters, status=False):
+    temp_df_list = []
+    for f in filters:
+        temp_df_list.append(df[df["title"].str.upper().str.contains(f.upper())])
 
-    dfs = []
-    for file in data_files:
-        df = pd.read_csv(f"data/responses/{file}")
-        dfs.append(df)
+    df = pd.concat(temp_df_list, ignore_index=True)
 
-    df = pd.concat(dfs, ignore_index=True)
     df = df[
-        (~df["title"].str.upper().str.contains("PLENO"))
-        & (~df["title"].str.upper().str.contains("SÊNIOR"))
-        & (~df["title"].str.upper().str.contains("SENIOR"))
-        & (~df["title"].str.upper().str.contains("SR"))
-        & (~df["title"].str.upper().str.contains("PL"))
-        & ((df["country"] == "Brasil") | (df["country"].isna()))
+        ((df["country"] == "Brasil") | (df["country"].isna()))
+        & (df["submitted"] == status)
     ]
-    df["date"] = pd.to_datetime(df["published_date"])
+
+    return df
+
+
+def apply_search_filters(df, filters):
+    temp_df_list = []
+    for f in filters:
+        temp_df_list.append(df[df["title"].str.upper().str.contains(f.upper())])
+
+    df_final = pd.concat(temp_df_list, ignore_index=True)
+    return df_final
+
+
+def update_google_sheets_dataset():
+    responses_datasets_filenames = os.listdir("data/responses/")
+
+    df_list = []
+    for filename in responses_datasets_filenames:
+        print(filename)
+        df_response = pd.read_csv(f"data/responses/{filename}")
+        df_list.append(df_response)
+
+    df = pd.concat(df_list, ignore_index=True)
+
+    df = apply_exclusion_filters(
+        df, ["PLENO", "SÊNIOR", "SENIOR", "SR", "PL", "III", "lll", "ll", "II"],
+        status=True,
+    )
+
     df = df.fillna("...")
+
+    df["date"] = pd.to_datetime(df["published_date"]).dt.strftime("%Y/%m/%d")
+    df["workplace_type"] = df["workplace_type"].replace(
+        {"hybrid": "2.Híbrido", "remote": "1.Remoto", "on-site": "3.Presencial"}
+    )
+    df = df.sort_values(
+        by=[
+            "date",
+            "workplace_type",
+        ],
+        ascending=[False, True],
+    )
+    df["date"] = pd.to_datetime(df["date"]).dt.strftime("%d/%m/%Y")
+    df["workplace_type"] = df["workplace_type"].apply(lambda x: x.split(".")[1])
+
     df = df[
         [
             "date",
@@ -108,18 +127,7 @@ def update_google_sheets_dataset():
             "state",
         ]
     ]
-    df["workplace_type"] = df["workplace_type"].replace(
-        {"hybrid": "2.Híbrido", "remote": "1.Remoto", "on-site": "3.Presencial"}
-    )
-    df = df.sort_values(
-        by=[
-            "date",
-            "workplace_type",
-        ],
-        ascending=[False, True],
-    )
-    df["date"] = df["date"].dt.strftime("%d/%m/%Y")
-    df["workplace_type"] = df["workplace_type"].apply(lambda x: x.split(".")[1])
+
     df.columns = [
         "Data",
         "Vaga",
@@ -133,7 +141,4 @@ def update_google_sheets_dataset():
 
 
 if __name__ == "__main__":
-    from pprint import pprint
-
-    data = DataHandler()
-
+    update_google_sheets_dataset()
