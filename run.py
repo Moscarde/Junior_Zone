f7fc320e6bbf493457b78560881633084f9b1b6b
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2024-10-15 07:33:50 -0300

    Daily update

--- run.py
+++ run.py
@@ -14,19 +14,25 @@ def main():
     process_and_publish_responses(MAIN_GROUP_CHAT_ID)
 
     update_sheets_dataset()
-    
-    subprocess.run(["git", "add", "."], check=True)
-    subprocess.run(["git", "commit", "-m", "Daily update"], check=True)
-    subprocess.run(["git", "push", "origin", "automation"], check=True)
+
+    try:
+        subprocess.run(["git", "add", "."], check=True)
+        subprocess.run(["git", "commit", "-m", "Daily update"], check=True)
+        subprocess.run(["git", "push", "origin", "automation"], check=True)
+    except subprocess.CalledProcessError as e:
+        print("Erro ao executar comandos git:", e)
+        return
 
     print(">> WAITING 20s FOR THE PROCESS TO FINISH")
     time.sleep(20)
 
     url = "https://bit.ly/planilhaJuniorZone1"
-    webbrowser.open(url)
+    try:
+        webbrowser.open(url)
+    except webbrowser.Error as e:
+        print("Erro ao abrir o navegador:", e)
 
     exit()
-
 
 if __name__ == "__main__":
     main()
